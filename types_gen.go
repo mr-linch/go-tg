76ee3e0b1ba66dafdb4b4979fb34c4837b091d1f
Sasha Savchuk <sasha@bots.house>
2022-06-28 02:04:51 +0300

    fix(types): InputMessageContent interface

--- types_gen.go
+++ types_gen.go
@@ -2262,21 +2262,6 @@ type InlineQueryResultCachedAudio struct {
 	InputMessageContent *InputMessageContent `json:"input_message_content,omitempty"`
 }
 
-// InputMessageContent this object represents the content of a message to be sent as a result of an inline query. Telegram clients currently support the following 5 types:
-type InputMessageContent struct {
-	// Text of the message to be sent, 1-4096 characters
-	MessageText string `json:"message_text"`
-
-	// Optional. Mode for parsing entities in the message text. See formatting options for more details.
-	ParseMode string `json:"parse_mode,omitempty"`
-
-	// Optional. List of special entities that appear in message text, which can be specified instead of parse_mode
-	Entities []MessageEntity `json:"entities,omitempty"`
-
-	// Optional. Disables link previews for links in the sent message
-	DisableWebPagePreview bool `json:"disable_web_page_preview,omitempty"`
-}
-
 // InputTextMessageContent represents the content of a text message to be sent as the result of an inline query.
 type InputTextMessageContent struct {
 	// Text of the message to be sent, 1-4096 characters
