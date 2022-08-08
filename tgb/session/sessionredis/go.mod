4e3fe92be0445249df404556f52728b6f183cd88
Sasha Savchuk <sasha@bots.house>
2022-08-08 17:33:22 +0300

    remove stores from source

--- tgb/session/sessionredis/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/mr-linch/go-tg/tgb/session/sessionredis
-
-go 1.18
--- tgb/session/sessionredis/store_test.go
+++
@@ -1,8 +0,0 @@
-package sessionredis
-
-// func TestV9(t *testing.T) {
-// 	var client *redis.Client
-
-// 	store := NewStore[*redis.StatusCmd, *redis.StringCmd, *redis.IntCmd](client)
-
-// }
