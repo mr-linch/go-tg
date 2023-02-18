fa9e92f1b8c094b1e863b43166088d547e07d469
Sasha Savchuk <sasha@bots.house>
2023-02-18 03:15:25 +0200

    tests(tg): tests of Request setters

--- request_test.go
+++ request_test.go
@@ -33,7 +33,7 @@ func TestRequest_Setters(t *testing.T) {
 	r.File("file_by_id", FileArg{
 		FileID: "file_id",
 	})
-	r.File("file_url", FileArg{
+	r.File("file_by_url", FileArg{
 		URL: "file_url",
 	})
 	r.File("file_input", FileArg{
@@ -64,6 +64,7 @@ func TestRequest_Setters(t *testing.T) {
 		"bool",
 		"chat",
 		"file_by_id",
+		"file_by_url",
 		"file_id",
 		"float64",
 		"foo",
@@ -81,7 +82,6 @@ func TestRequest_Setters(t *testing.T) {
 		"attachment_0_thumb",
 		"file",
 		"file_input",
-		"file_url",
 	}, encoder.fileKeys)
 }
 
