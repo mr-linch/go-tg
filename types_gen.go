b010a13ef683d4a04456260157f68d1452102dbe
Oleksandr Savchuk <sasha@bots.house>
2024-02-18 14:50:35 +0200

    fix UsersShared.UserIDs naming

--- types_gen.go
+++ types_gen.go
@@ -1066,7 +1066,7 @@ type UsersShared struct {
 	RequestID int `json:"request_id"`
 
 	// Identifiers of the shared users. These numbers may have more than 32 significant bits and some programming languages may have difficulty/silent defects in interpreting them. But they have at most 52 significant bits, so 64-bit integers or double-precision float types are safe for storing these identifiers. The bot may not have access to the users and could be unable to use these identifiers, unless the users are already known to the bot by some other means.
-	UserIds []int `json:"user_ids"`
+	UserIDs []UserID `json:"user_ids"`
 }
 
 // ChatShared this object contains information about the chat whose identifier was shared with the bot using a KeyboardButtonRequestChat button.
